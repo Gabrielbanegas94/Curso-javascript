eca5b2a130a797404062267eb64150c59cd73895
gabriel <banegasgabriel@gmail.com>
2020-01-17 00:24:59 -0300

    agregamos busqueda con indice

--- Hola-mundo/js/26-array-multi.js
+++ Hola-mundo/js/26-array-multi.js
@@ -5,4 +5,28 @@ var peliculas =["hellowen", "jojo rabbit", "1917"];
 
 var cine = [categorias,peliculas];
 
-console.log(cine);
+//console.log(cine);
+/*var elemento ="";
+/*while(elemento != "acabar")
+{
+    peliculas.push(elemento);//añade elemento al array
+    elemento =prompt("Introduce la pelicula");
+
+}*/
+
+/*do{
+    elemento =prompt("Introduce la pelicula");
+    peliculas.push(elemento);
+
+}while(elemento != "acabar")
+peliculas.pop();
+
+*/
+var indice = peliculas.indexOf("jojo rabbit");
+if(indice > -1)
+{
+    peliculas.splice(indice,1);//averiguamos del indice una vez encontrado
+}
+
+var peliculaString=peliculas.join();//convierte las peliculas a texto
+console.log(peliculaString);
