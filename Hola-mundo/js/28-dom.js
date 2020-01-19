467a78e13009b96cf5e3803455d85009274d782d
gabriel <banegasgabriel@gmail.com>
2020-01-19 20:08:14 -0300

    no sale lo del section

--- Hola-mundo/js/28-dom.js
+++ Hola-mundo/js/28-dom.js
@@ -1,16 +1,40 @@
 'use strict'
+//DOM
 
-
+//con id concreto
 //var caja = document.getElementById("caja");
 var caja=document.querySelector("#caja");
-caja.innerHTML="texto desde la cajaj js";
+caja.innerHTML="texto desde la caja js";
 caja.style.background= "red";
 caja.style.padding= "20px";
 caja.className="hola";
 
-function cambiacolor(color)
+/*function cambiacolor(color, cambio=true)
 {
-    caja.style.background=color;
+    if(cambio==true)
+    {
+     caja.style.background=color;
+    }
+}*/
+
+//conseguir por su etiqueta
+var todoslosdivs= document.getElementsByTagName("div");//crea un array de divs
+
+//var contenidoentexto=todoslosdivs[2];
+
+//contenidoentexto.innerHTML="otro valor";//meto contendio
+
+
+var valor;
+for(valor in todoslosdivs)
+{
+   if(typeof todoslosdivs[valor].textContent == 'String')
+   {
+    var parrafo = document.createElement("p");//crea un parrafo
+    var texto = document.createTextNode(todoslosdivs[valor].textContent);//valor de cada uno de los divs
+    parrafo.appendChild(texto);
+    document.querySelector("#miseccion").appendChild(parrafo);
+   }
 }
-cambiacolor("green");
-console.log(caja);
+//console.log(contenidoentexto);
+
