c6f6c9ebb064d4ec28088deed34fe8287c994335
gabriel <banegasgabriel@gmail.com>
2020-01-19 21:10:44 -0300

    arreglado lo de section

--- Hola-mundo/js/28-dom.js
+++ Hola-mundo/js/28-dom.js
@@ -24,17 +24,38 @@ var todoslosdivs= document.getElementsByTagName("div");//crea un array de divs
 
 //contenidoentexto.innerHTML="otro valor";//meto contendio
 
-
+var seccion= document.querySelector("#miseccion");
 var valor;
 for(valor in todoslosdivs)
 {
-   if(typeof todoslosdivs[valor].textContent == 'String')
-   {
+    
+    if(typeof todoslosdivs[valor].textContent=="string")
+    {
     var parrafo = document.createElement("p");//crea un parrafo
     var texto = document.createTextNode(todoslosdivs[valor].textContent);//valor de cada uno de los divs
-    parrafo.appendChild(texto);
-    document.querySelector("#miseccion").appendChild(parrafo);
-   }
+    parrafo.append(texto);
+    seccion.append(parrafo);
+    }
+   
 }
 //console.log(contenidoentexto);
 
+//***********por class */
+var divRojos= document.getElementsByClassName("rojo");
+var divAmarillos= document.getElementsByClassName("amarillo");
+console.log(divAmarillos);
+divAmarillos[0].style.background = "yellow";
+var div;
+for(div in divRojos){
+    if( divRojos[div].className=="rojo"){
+
+        divRojos[div].style.background="red";
+    }
+}
+//usamos query selector
+var id = document.querySelector("#encabezado");
+console.log(id);
+//*** */
+var claseRojo = document.querySelectorAll("div.rojo");///sellecciona todos los elementos con div y clase rojo
+console.log(claseRojo);
+console.log(divRojos);
