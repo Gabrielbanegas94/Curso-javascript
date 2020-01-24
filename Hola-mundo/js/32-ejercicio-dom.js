7314ccfd3fe6675edd110d4d152f70af55a17820
gabriel <banegasgabriel@gmail.com>
2020-01-24 00:19:06 -0300

    agregamos la validacion del formulario

--- Hola-mundo/js/32-ejercicio-dom.js
+++ Hola-mundo/js/32-ejercicio-dom.js
@@ -6,15 +6,35 @@ window.addEventListener("load", () => {
     box_dashed.style.display ="none";//no lo muestra
     formulario.addEventListener('submit', () => {
         console.log("evento capturado");
-        box_dashed.style.display ="block";//cuando meto la info lo muestra
         var nombre = document.querySelector("#nombre").value;///toma el valor que tiene dentro
         var apellido = document.querySelector("#apellido").value;
         var edad = document.querySelector("#edad").value;
-        
-       var p_nombre= document.querySelector("#p_nombre span");
+         //***validacion del formulario */
+        if(nombre.trim()==null || nombre.trim().length==0 ||!isNaN(nombre))
+        {
+            alert("el nombre es incorrecto");
+            document.querySelector("#error_nombre").innerHTML="el nombre no es valido";
+            return false;
+        }
+
+      //************ */
+        if(apellido.trim()==null || apellido.trim().length==0 || !isNaN(apellido))
+        {
+            alert("el apellido es incorrecto");
+            return false;
+        }
+     //********************** */
+        if(edad.trim()==null || edad <=0 || isNaN(edad) )
+        {
+            alert("la edad es incorrecta");
+            return false;
+        }
+
+        var p_nombre= document.querySelector("#p_nombre span");
         var p_apellido= document.querySelector("#p_apellido span");
         var p_edad= document.querySelector("#p_edad span");
-    
+        
+        box_dashed.style.display ="block";//cuando meto la info lo muestra
         
         p_nombre.innerHTML=nombre;
         p_apellido.innerHTML=apellido;
