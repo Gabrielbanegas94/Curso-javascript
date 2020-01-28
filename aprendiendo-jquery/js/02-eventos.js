45f2887546256494b664eb831a9d41807139000e
gabriel <banegasgabriel@gmail.com>
2020-01-27 23:36:52 -0300

    agregamos hasta eventos blur

--- aprendiendo-jquery/js/02-eventos.js
+++ aprendiendo-jquery/js/02-eventos.js
@@ -27,4 +27,13 @@ $(document).ready(function () {
         $(this).css("background","pink")
                 .css("color","yellow");
     });
+   var nombre=$("#nombre");
+    nombre.focus(function(){
+       $(this).css("border","2px solid green");
+    });
+
+    nombre.blur(function(){
+        $(this).css("border","1px solid grey");
+        $("#datos").text( $(this).val()).show();//selleciona el valor y lo muestra
+     });
 });
