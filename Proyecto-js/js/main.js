38089fc527ac12663162db411929245fcc7dde00
gabriel <banegasgabriel@gmail.com>
2020-03-05 20:55:33 -0300

    agrege reloj y form de  contacto

--- Proyecto-js/js/main.js
+++ Proyecto-js/js/main.js
@@ -95,8 +95,35 @@ var about_parrafo = $("#about p");
   });
 
 }
+//Acordeon
   if (window.location.href.indexOf('about') > -1) {
     $("#acordeon").accordion();
   }
+
+  // Reloj
+	if(window.location.href.indexOf('reloj') > -1){
+
+		setInterval(function(){
+				var reloj = moment().format("hh:mm:ss");
+				$('#reloj').html(reloj);
+		}, 1000);
+		
+	
+  }
+  
+  // Validación
+	if(window.location.href.indexOf('contact') > -1){
+	
+		$("form input[name='date']").datepicker({
+			dateFormat: 'dd-mm-yy'
+		});
+
+		$.validate({
+		    lang: 'es',
+		    errorMessagePosition: 'top',
+		    scrollToTopOnError: true
+		});
+
+	}
 
 });
