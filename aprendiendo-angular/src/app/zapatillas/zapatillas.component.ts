fa649bb0b4042e09372f64146538bcba05a84961
gabriel <banegasgabriel@gmail.com>
2020-03-16 16:03:04 -0300

    agregamos condiconal ngfor y ngswitch

--- aprendiendo-angular/src/app/zapatillas/zapatillas.component.ts
+++ aprendiendo-angular/src/app/zapatillas/zapatillas.component.ts
@@ -10,19 +10,37 @@ export class zapatillasComponent implements OnInit{
 
     public titulo:string ="componente de zapatillas";
     public zapatillas : Array<Zapatilla>;
+    public marcas : string[];
+    public color: string;
 
     constructor()
     {
+        this.color="red";
+        this.marcas =new Array();
         this.zapatillas = [
-            new Zapatilla('Rebbok clasicc','rebook','verde canario',80,true),
+            new Zapatilla('Rebbok clasicc','rebook','verde canario',80,false),
             new Zapatilla('Nike Air','Nike','gris',100,true),
-            new Zapatilla('clasicc','adidas','violeta',50,true)
+            new Zapatilla('clasicc','adidas','violeta',50,false),
+            new Zapatilla('Underground','Under Armour','blancas',70,true),
+            new Zapatilla('Nike Air','Nike','gris',100,true),
         ];
     }
 
     ngOnInit(){
         console.log(this.zapatillas);
+        this.GetMarcas();
 
+
+    }
+    GetMarcas(){
+        this.zapatillas.forEach((zapatilla,index)=>{
+            if(this.marcas.indexOf(zapatilla.marca)<0)
+            {//si no esta en el array lo inserta por nombre de marca
+            this.marcas.push(zapatilla.marca);//meto en el vector
+            
+               }
+        });
+        console.log(this.marcas);
     }
 
 }
